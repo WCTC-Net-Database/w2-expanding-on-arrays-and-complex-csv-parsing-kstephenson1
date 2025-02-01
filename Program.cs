f046c971930043eddfdb402c462e7e3e41330992
Kyle S <kstephenson1@my.wctc.edu>
2025-02-01 00:49:38 -0600

    Refactored UI into Interface

--- Program.cs
+++ Program.cs
@@ -9,42 +9,26 @@ class Program
         // Will run until exit is selected
         while (true)
         {
-            ShowMenu();
+            Interface.MainMenu();
+
             int selection = Input.GetInt(1, 4, "Value must be between 1-4"); // Uses a helper file to get an int between 1-4 from the user
+
             if (selection == 4) break; // Exits the program if '4' is selected.
+
             switch (selection) // Checks the input from the user and responds appropriately.
             {
                 case 1:
-                    CharacterInfo.DisplayAllCharacters();
+                    CharacterHandler.DisplayAllCharacters();
                     break;
                 case 2:
-                    CharacterInfo.NewCharacter();
+                    CharacterHandler.NewCharacter();
                     break;
                 case 3:
-                    CharacterInfo.LevelUp();
+                    CharacterHandler.LevelUp();
                     break;
             }
         }
 
-        Console.Clear();
-        Console.WriteLine(
-            "\n\n\n\n\n\n\n\n\n" +
-            "          ╔═════════════════════════════════════════════════════╗\n" +
-            "          ║    Thank you for using the RPG Character Editor.    ║\n" +
-            "          ╚═════════════════════════════════════════════════════╝\n" +
-            "\n\n\n\n\n\n\n\n\n");
-    }
-
-    static void ShowMenu()
-    {
-        Console.WriteLine(
-            "╔═════════════════════════╗\n" +
-            "║        Main Menu        ║\n" +
-            "╠═════════════════════════╣\n" +
-            "║1. Display Characters    ║\n" +
-            "║2. New Character         ║\n" +
-            "║3. Level Up Character    ║\n" +
-            "║4. Exit                  ║\n" +
-            "╚═════════════════════════╝\n");
+        Interface.ExitMenu();
     }
 }
