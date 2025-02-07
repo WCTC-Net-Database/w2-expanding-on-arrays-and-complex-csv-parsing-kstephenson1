7340e4aab2583ad363f53654a2f92a5cee90768e
Kyle S <kstephenson1@my.wctc.edu>
2025-02-07 01:45:14 -0600

    Comment added - is not implemented yet

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿namespace w2_assignment_ksteph.Inventory;
 public class Inventory
 {
+    // Is not really implemented yet but will be on w3 assignment.
     public List<Item>? Items { get; set; }
 }
