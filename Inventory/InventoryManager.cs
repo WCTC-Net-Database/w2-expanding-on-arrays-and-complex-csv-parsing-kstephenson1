0e45667382e6f2c7ec0e707b953c481cecedc4e1
Kyle S <kstephenson1@my.wctc.edu>
2025-02-07 01:45:54 -0600

    Cleaned up namespace and using

--- Inventory/InventoryManager.cs
+++ Inventory/InventoryManager.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace w2_assignment_ksteph.Inventory;
 
-namespace w2_assignment_ksteph.Inventory;
+using System;
+using System.Collections.Generic;
+
 
 public class InventoryManager
 {
